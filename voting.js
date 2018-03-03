642f194aeb7a5ef77a178567a0a22aed883b19a3
Sarah <gtsli469@gmail.com>
2018-03-03 01:17:46 -0500

    created video player and vote system

---
+++ voting.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Vote For Your Favorite Song</h1>
+<p>My First Paragraph</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+</script>
+
+</body>
+</html>
